6f4353d380c67b33b7e47346f9abe10ae691b54a
Oathian <aidanmaycock@gmail.com>
2022-08-12 00:27:27 +0100

    sign styling

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import  { getUserByUsername, postUser }  from "../utils/api";
 import { useNavigate } from "react-router-dom";
 import ErrorComponent from "./ErrorComponent";
+import '../styles/SignIn.css';
 
 const SignIn = ({ setUser }) => {
     const [toggleSign, setToggleSign] = useState("Sign In");
@@ -25,7 +26,6 @@ const SignIn = ({ setUser }) => {
 
     const handleSignUp = (event) => {
         event.preventDefault();
-        console.log(event.target[0].value, event.target[1].value, event.target[2].value);
         postUser(event.target[0].value, event.target[1].value, event.target[2].value).then((user) => {
             console.log(user)
             setUser(user);
@@ -50,25 +50,27 @@ const SignIn = ({ setUser }) => {
     };
 
     return (
-        <section>
-            <button onClick={handleToggle}>{toggleSign === "Sign In"?"Sign Up":"Sign In"}</button>
-            {toggleSign==="Sign In"?<form onSubmit={handleSignIn}>
-                <h3>Sign In</h3>
+        <section className="signPage">
+            <section className="signBox">
+            <button className="signToggle" onClick={handleToggle}>{toggleSign === "Sign In"?"Sign Up":"Sign In"}</button>
+            {toggleSign==="Sign In"?<form className="signForm" onSubmit={handleSignIn}>
+                <h3 className="signTitle">Sign In</h3>
                 {error?<ErrorComponent error={error}/>:<></>}
                 <label htmlFor="username">Username:</label>
-                <input value={username} onChange={(event) => setUsername(event.target.value)} id="username"></input>
-                <button type="sumbit">Submit</button>
-            </form>:<form onSubmit={handleSignUp}>
-                <h3>Sign Up</h3>
+                <input className="signInput" value={username} onChange={(event) => setUsername(event.target.value)} id="username"></input>
+                <button className="signButton" type="sumbit">Submit</button>
+            </form>:<form className="signForm" onSubmit={handleSignUp}>
+                <h3 className="signTitle">Sign Up</h3>
                 {error?<ErrorComponent error={error}/>:<></>}
                 <label htmlFor="username">Username:</label>
-                <input value={username} onChange={(event) => setUsername(event.target.value)} id="username"></input>
+                <input className="signInput" value={username} onChange={(event) => setUsername(event.target.value)} id="username"></input>
                 <label htmlFor="name">First Name:</label>
-                <input value={name} onChange={(event) => setName(event.target.value)} id="name"></input>
+                <input className="signInput" value={name} onChange={(event) => setName(event.target.value)} id="name"></input>
                 <label htmlFor="avatar_url">Avatar Url:</label>
-                <input value={avatar_url} onChange={(event) => setAvatar_url(event.target.value)} id="avatar_url"></input>
-                <button type="sumbit">Submit</button>
+                <input className="signInput" value={avatar_url} onChange={(event) => setAvatar_url(event.target.value)} id="avatar_url"></input>
+                <button className="signButton" type="sumbit">Submit</button>
             </form>}
+            </section>
         </section>)
 };
 
